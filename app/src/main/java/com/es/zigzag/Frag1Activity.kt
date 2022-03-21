34c77333b2066afd48f00fa47e5c61dae7d0107c
기은서 <ellen1987@naver.com>
2022-03-22 01:58:24 +0900

    쇼핑몰 만드는 앱 구현 -fragment 첫 구현

---
+++ app/src/main/java/com/es/zigzag/Frag1Activity.kt
@@ -0,0 +1,26 @@
+package com.es.zigzag
+
+import android.graphics.Color
+import android.os.Bundle
+import android.text.Spannable
+import android.text.SpannableStringBuilder
+import android.text.style.ForegroundColorSpan
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import androidx.fragment.app.Fragment
+
+class Frag1Activity:  Fragment() {
+    private lateinit var btn6 : Button
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        var view : View = inflater.inflate(R.layout.activity_frag1, container, false)
+        btn6 = view.findViewById(R.id.btn6)!!
+        var ssb = SpannableStringBuilder(btn6.text);
+        ssb.setSpan(ForegroundColorSpan(Color.parseColor("#696870")), 14, btn6.text.length, Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
+        btn6.text = ssb
+
+        return view
+    }
+}
---
+++ app/src/main/java/com/es/zigzag/MainActivity.kt
@@ -0,0 +1,27 @@
+package com.es.zigzag
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
+import androidx.viewpager.widget.ViewPager
+import com.google.android.material.tabs.TabLayout
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var viewPager: ViewPager
+    private lateinit var tabLayout: TabLayout
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        viewPager = findViewById(R.id.viewPager)
+        tabLayout = findViewById(R.id.tabLayout)
+        val adapter : PagerAdapter = PagerAdapter(supportFragmentManager)
+        adapter.addFragment(Frag1Activity(), "홈")
+        viewPager.adapter = adapter
+        tabLayout.setupWithViewPager(viewPager)
+        }
+
+    }
